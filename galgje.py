c5cfc81fbe63066928853313ca9924c0dfc3ec23
Israh <israh.emara@coderclass.nl>
2017-10-05 09:57:16 +0200

    Basis vragen gemaakt voor het spel galgje

--- galgje.py
+++ galgje.py
@@ -1,1 +1,79 @@
-print ("hello world!")
+woord = ["huis"]
+ggl = ["",""]
+gl = ["",""]
+fouten = 0
+print ("Welkom bij ")
+print("hier komt galgje")
+
+
+
+
+vraag1 = input(str("Kies een letter of kies een ? om het woord te raden."))
+if vraag1 > 1:
+    print("Vul aub 1 letter in of een ? om het woord te raden.")
+print("Dit zijn de goed geraden letters."ggl)
+print("Dit alle geraden letters."gl)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+woordenlijst= lijst met geheime woorden geheim woord wordt bepaald = bijvoorbeeld appel
+functie=aantalFouten om fouten te tellen - aantalFouten = 0
+functie tekenGalg(aantalFouten): met stappen gelijk aan fouten.
+stap 1:teken stap1
+------------------------
+stap 2: teken stap2
+------------------------
+stap3: teken stap3
+------------------------
+stap4: teken stap4
+------------------------
+stap5: teken stap5
+------------------------
+
+functie ABC= houdt bij welke letters al ingevoerd zijn.
+
+functie Letter= vraag om  1 letter in te typen.= print"Typ 1 letter in"
+
+functie punten= houdt aantal punten per spel dat je speelt bij
+
+functie highscore= telt aantal punten van het spel waar je de meeste punten hebt behaald op als "punten">"highscore" maak "punten"="highscore"
+als Letter="?" dan mag speler hele woord intypen als speler niet goede woord intypt print"helaas dat is niet het goede woord!"
+functie break= als typ in break= stop spel
+
+while loop:
+	print"Letter"+print"of een ?"
+	als letter fout maak galgje stap 1(aantal fouten meegeven)+schrijf letter op en
+		print"Helaas, fout!"
+
+	als speler iets anders dan 1 letter intypt dan geef foutmelding+ print"kies 1 letter"
+
+	als letter goed schrijf letter op bij de streepjes en print"Goed geraden!"
+
+
+	als galg stap 5 is klaar print"helaas je hangt aan de galg"+ print"woord
+	als "?" intyp print"raad woord" als woord goed geraden print"goed geraden" anders 	print"fout ga door"
+	als woord geraden print"Ja goed geraden"+print"woord"
+	als woord geraden print"Je hebt 50 punten"+ begint nieuw spel met nieuw woord.
+
+
+
+
+
+
+"""
